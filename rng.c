3ba12df6f935921d75d7528cbf6e4b133f6349e0
MDWooper <kemokemo6630@yahoo.com.tw>
2016-05-11 23:23:13 -0400

    all function working correctly

--- rng.c
+++ rng.c
@@ -1,29 +1,52 @@
+/*
+Code written by: Joanna Hsiao, Leila Hsiao, Ian McMahon, Briana Lamet, Abraham Arce
+
+Usage:\n\
+        Enter [postive number x]:\n\
+        get the generation time of x random number.\n\
+        Ex: ./rng 10000\n\
+        \n\
+        Enter 't':\n\
+        get a table of generation time of fix number\n\
+        Ex: ./rng t
+        \n\
+        Enter 'ta' [postive number y]:\n\
+        to get a table of average generation time of fix numbers ran y time\n\
+        Ex: ./rng ta 100
+        \n\
+        Enter 'a' [postive number x] [postive number y]:\n\
+        to get the average generation time of x number ran y time\n\
+        Ex: ./rng a 10000 100\n\
+
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/*TODO: 
-2. A mode that calculate time spent to run 100, 1000, 10000.... in one single run
-3. A mode that calcutate the average time spent to generate x time of y amount random number
-4. A mode that does samething as 2 but calculate in x run and calculate average time
+/*
+getRnGenTime(): calculate time spent to generate x of random number
+genTableTime(): A mode that calculate time spent to run 100, 1000, 10000.... in one single run
+genSingleAveTime(): A mode that calcutate the average time spent to generate x time of y amount random number
+genTableAveTime(): A mode that does samething as 2 but calculate in x run and calculate average time
 */
+
 clock_t getRnGenTime(long long RNamount);
 void genTableTime();
 void genTableAveTime(long long timeToGen);
-void genSingleAveTime();
+void genSingleAveTime(long long amount, long long timeToGen);
 void printHelp();
 
 // usage: ./rng [argument]
-//	argument must be postive number
-//	right now this function calculate time spent to generate [argument] random number.
+
 int main(int argc, char *argv[])
 {
 
 	long long amount;
 	long long timeToGen;	
 	double time;
-	
-	
 
 	if(argc < 2 || argc > 4)
 	{
@@ -37,8 +60,8 @@ int main(int argc, char *argv[])
 			genTableTime();
 		else if (argc == 3 && strcmp("ta",argv[1]) == 0 && ( timeToGen = atoll(argv[2]) ) > 0)
 			genTableAveTime(timeToGen);
-		else if (argc == 3 && strcmp("a",argv[1]) && (amount = atoll(argv[2])) > 0 && (timeToGen = atoll(argv[3]			)) > 0)
-			genSingleAveTime();
+		else if (argc == 4 && strcmp("a",argv[1]) == 0 && (amount = atoll(argv[2])) > 0 && (timeToGen = atoll(argv[3]			)) > 0)
+			genSingleAveTime(amount, timeToGen);
 		else
 		{
 			printHelp();
@@ -49,6 +72,11 @@ int main(int argc, char *argv[])
 	{
 		time = (double)(getRnGenTime(amount));
 		printf("\nTime spent to generate %lld random number: %f clock tick(s)\n", amount, time);
+	}
+	else
+	{
+		printHelp();
+		exit(1);
 	}
 	return 0;
 }
@@ -84,8 +112,18 @@ void genTableAveTime(long long timeToGen)
 	}
 }
 
-void genSingleAveTime()
-{}
+void genSingleAveTime(long long amount, long long timeToGen)
+{
+	double avetime;
+	int i;
+
+		for(i = 0; i < timeToGen; i++)
+			avetime = avetime + (double)(getRnGenTime(amount));
+
+                avetime = avetime / (double)(timeToGen);
+                printf("\nAverage Time spent to generate %lld random number for %lld time: %f clock tick(s)\n",amount,timeToGen,avetime);
+}
+
 
 // this function calculate time spent to generate RNamount of random number
 clock_t getRnGenTime (long long RNamount)
@@ -111,15 +149,19 @@ void printHelp()
 Usage:\n\
 	Enter [postive number x]:\n\
 	get the generation time of x random number.\n\
+	Ex: ./rng 10000\n\
 	\n\
 	Enter 't':\n\
 	get a table of generation time of fix number\n\
+	Ex: ./rng t
 	\n\
 	Enter 'ta' [postive number y]:\n\
 	to get a table of average generation time of fix numbers ran y time\n\
+	Ex: ./rng ta 100
 	\n\
 	Enter 'a' [postive number x] [postive number y]:\n\
 	to get the average generation time of x number ran y time\n\
+	Ex: ./rng a 10000 100\n\
 ");
 
 }
